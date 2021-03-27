c10904b753240ee45475fe3012e2535c738b6875
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-03-27 12:37:56 -0600

    Rework MovieDetailsCard into a class component. Implement second network request to obtain additional movie details

--- src/details/MovieDetailsCard.js
+++ src/details/MovieDetailsCard.js
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { fetchMovieDetails } from '../APICalls'
 import './MovieDetailsCard.css'
 
-const MovieDetailsCard = ({movie, displayMovieLibrary}) => {
-  console.log(movie)
-  return (
-    <div className='movie-details-card' style={{backgroundImage: `url(${movie.backdrop_path})`}}>
-      <div className='movie-details-text'>
-        <button className='close-button' onClick={() => displayMovieLibrary()}>X</button>
-        <h2>{movie.title}</h2>
-        <p>RELEASE DATE: {movie.release_date}</p>
-        <p>AVERAGE RATING: {movie.average_rating.toFixed(2)}</p>
+class MovieDetailsCard extends Component {
+  constructor({ movie, displayMovieLibrary }) {
+    super()
+    this.state = {
+      displayMovieLibrary: displayMovieLibrary,
+      movie: movie
+    }
+  }
+
+  componentDidMount() {
+    fetchMovieDetails(this.state.movie.id)
+    .then(movieDetails => {
+      this.setState({
+        movie: movieDetails.movie
+      })
+    })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className='movie-details-card' style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}}>
+       <div className='movie-details-text'>
+         <button className='close-button' onClick={() => this.state.displayMovieLibrary()}>X</button>
+         <h2>{this.state.movie.title}</h2>
+         <p>RELEASE DATE: {this.state.movie.release_date}</p>
+         <p>AVERAGE RATING: {this.state.movie.average_rating.toFixed(2)}</p>
       </div>
-    </div>
-  )
+     </div>
+    )
+  }
+
 }
+
+// const MovieDetailsCard = ({movie, displayMovieLibrary}) => {
+//   console.log(movie)
+//   return (
+//     <div className='movie-details-card' style={{backgroundImage: `url(${movie.backdrop_path})`}}>
+//       <div className='movie-details-text'>
+//         <button className='close-button' onClick={() => displayMovieLibrary()}>X</button>
+//         <h2>{movie.title}</h2>
+//         <p>RELEASE DATE: {movie.release_date}</p>
+//         <p>AVERAGE RATING: {movie.average_rating.toFixed(2)}</p>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default MovieDetailsCard
