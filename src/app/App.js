f3060d8dd86fba8240bd1b216b8f2dec82959de7
Jessica Justice <jessbjust@gmail.com>
2021-04-03 14:50:24 -0600

    Fix bug that cleared search input

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { fetchAllMovies } from '../APICalls'
+import Form from '../search/Form.js'
 import MovieContainer from '../container/MovieContainer'
 import MovieDetailsCard from '../details/MovieDetailsCard';
 
@@ -23,14 +24,16 @@ export default class App extends Component {
     return (
       <main className="App">
         <h1>Rancid Tomatillos</h1>
+        <Form className="search-bar" filterMovies={this.filterMovies} />
         <Switch>
           <Route exact path="/" render={() =>
+
             <MovieContainer
               movies={this.state.searchResults || this.state.movies}
               filterMovies={this.filterMovies}
               isLoaded={this.state.isLoaded}
-            /> }
-          />
+            />
+          } />
           <Route path="/:id" render={(props) => <MovieDetailsCard movieID={props.match.params.id} /> } />
         </Switch>
       </main>
